185ac4bb6f0e66b6b1950afacde0dc0f82fed1f4
Bruno Muller <muller000@gmail.com>
2020-09-01 01:48:39 -0300

    Udemy Exercicios 2

---
+++ AprendendoCsharp/5-UdemyExercicios/5-UdemyExercicios/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_UdemyExercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Insira a primeira nota: ");
+              string nota1 = Console.ReadLine();
+              double.Parse(nota1);
+            Console.Write("Insira a segunda nota: ");
+              string nota2 = Console.ReadLine();
+              double.Parse(nota2);
+
+            double resultado = (double.Parse(nota1) + double.Parse(nota2)) / 2;
+
+            Console.WriteLine("A média entre " + nota1 + " e " + nota2 + " é " + resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AprendendoCsharp/5-UdemyExercicios/6-UdemyExercicios/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_UdemyExercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Digite um numero: ");
+            string n1 = Console.ReadLine();
+            int.Parse(n1);
+
+            int abaixo = int.Parse(n1) - 1;
+            Console.WriteLine("O antecessor de " + int.Parse(n1) + " é " + abaixo);
+
+            int acima = int.Parse(n1) + 1;
+            Console.WriteLine("O sucessor de " + int.Parse(n1) + " é " + acima);
+
+            Console.ReadLine();
+        }
+    }
+}
