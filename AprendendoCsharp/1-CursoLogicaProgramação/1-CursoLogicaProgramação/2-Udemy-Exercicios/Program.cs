8520d7ca345ce64177c7e21bcfd64c68fe30e3df
Bruno Muller <muller000@gmail.com>
2020-08-31 22:02:04 -0300

    udemy-entrada de dados 31/08

---
+++ AprendendoCsharp/1-CursoLogicaProgramação/1-CursoLogicaProgramação/2-Udemy-Exercicios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2_Udemy_Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = Console.ReadLine();
+        }
+    }
+}
