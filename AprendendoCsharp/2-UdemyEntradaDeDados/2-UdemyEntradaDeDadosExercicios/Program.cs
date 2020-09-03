be5f3f3d250e08120a08ccb518b54b4d2e47daa6
Bruno Muller <muller000@gmail.com>
2020-09-02 22:28:45 -0300

    Udemy Entrada de dados 2 - 02/09

---
+++ AprendendoCsharp/2-UdemyEntradaDeDados/2-UdemyEntradaDeDadosExercicios/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace _2_UdemyEntradaDeDadosExercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Entre seu nome completo: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("quantos quartos tem na sua casa?: ");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);    
+            Console.WriteLine("entre seu ultimo nome, idade e altura (mesma linha)");
+            string[] vet = Console.ReadLine().Split(' ');
+            string sobrenome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco);
+            Console.WriteLine(sobrenome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+            ;
+        }
+    }
+}
---
+++ AprendendoCsharp/7-UdemyExercicios/7-UdemyExercicios/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_UdemyExercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int A = 10, B = 20;
+
+            A = B;
+            B = A;
+
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+
+            Console.ReadLine();
+
+        }
+    }
+}
