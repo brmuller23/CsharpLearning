06b6504f218bbe37b702e53aa450a669e9bcfce8
Bruno Muller <muller000@gmail.com>
2020-09-16 00:02:05 -0300

    Curso em video 04 - 15/09

---
+++ CursoEmVideo-04/CursoEmVideo-04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoEmVideo_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+
+            A = 2;
+            B = 3;
+            C = 5;
+
+            Console.WriteLine(1=C%2);
+
+            Console.ReadLine();
+
+             
+        }
+    }
+}
