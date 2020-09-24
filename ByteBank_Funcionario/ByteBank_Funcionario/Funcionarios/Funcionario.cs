aa610cb9717a4b15dc8d21f0fef5129a03a141ea
Bruno Muller <muller000@gmail.com>
2020-09-23 22:38:57 -0300

    byte bank aula 03 - 23/09

--- ByteBank_Funcionario/ByteBank_Funcionario/Funcionarios/Funcionario.cs
+++ ByteBank_Funcionario/ByteBank_Funcionario/Funcionarios/Funcionario.cs
@@ -9,33 +9,15 @@ namespace ByteBank_Funcionario.Funcionarios
    public class Funcionario
 
     {
-        // 0 - funcionario
-        // 1 - diretor
-        // 2 - Designer
-        // 3 -  Gerente de conta corrente
-        // 4 - Coordenador
-
-        
-        private int _tipo;
-
-
 
         public string nome { get; set; }
         public string CPF { get; set; }
         public double Salario { get; set; }
-
-        public Funcionario(int tipo)
+        public virtual double GetBonificacao()
         {
-            _tipo = tipo;
-        }
-
-        public double GetBonificacao()
-        {
-            if (_tipo == 1)
-                return Salario;
-
-
+          
             return Salario * 0.10;
+
         }
     }
 }
--- ByteBank_Funcionario/ByteBank_Funcionario/Program.cs
+++ ByteBank_Funcionario/ByteBank_Funcionario/Program.cs
@@ -12,13 +12,37 @@ namespace ByteBank_Funcionario
         static void Main(string[] args)
 
         {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 
-            Funcionario carlos = new Funcionario(1);
+            Funcionario carlos = new Funcionario();
             carlos.nome = "Carlos";
             carlos.CPF = "047.159.759-77";
             carlos.Salario = 2000;
-                
-           
+
+            gerenciador.Registrar(carlos);
+
+
+            Diretor roberta = new Diretor();
+            roberta.nome = "Roberta";
+            roberta.CPF = "047.159.700-77";
+            roberta.Salario = 5000;
+
+         
+
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine(carlos.nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.WriteLine(roberta.nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total de Bonificações: " + gerenciador.GetTotalBonificacao());
+            Console.ReadLine();
+
+
+
         }
+
     }
 }
